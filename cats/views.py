0836dda6b2b5ee26301fe5efa36d1e502eb335ac
Banes31 <banes31@mail.ru>
2022-04-07 21:03:34 +0300

    first commit

--- cats/views.py
+++ cats/views.py
@@ -17,3 +17,16 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])  # Применили декоратор и указали разрешённые методы
+def hello(request):
+    # По задумке, в ответ на POST-запрос нужно вернуть JSON с теми данными,
+    # которые получены в запросе.
+    # Для этого в объект Response() передаём словарь request.data.
+    if request.method == 'POST':
+        return Response({'message': 'Получены данные', 'data': request.data})
+
+    # В ответ на GET-запрос нужно вернуть JSON
+    # Он тоже будет создан из словаря, переданного в Response()
+    return Response({'message': 'Это был GET-запрос!'})
