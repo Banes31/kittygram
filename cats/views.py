af07039e9d53308b89da79c0c033884549ad3d08
Banes31 <banes31@mail.ru>
2022-04-10 15:14:04 +0300

    third commit

--- cats/views.py
+++ cats/views.py
@@ -8,10 +8,13 @@ from .serializers import CatSerializer
 
 @api_view(['GET', 'POST'])
 def cat_list(request):
+    """В случае POST-запроса создается объект класса Cat.
+    В случае GET-запроса возвращается информация о всех объектах класса Cat.
+    """
     if request.method == 'POST':
         # Создаём объект сериализатора
         # и передаём в него данные из POST-запроса
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=False)
         if serializer.is_valid():
             # Если полученные данные валидны —
             # сохраняем данные в базу через save().
@@ -29,6 +32,7 @@ def cat_list(request):
 
 @api_view(['GET', 'POST'])  # Применили декоратор и указали разрешённые методы
 def hello(request):
+    """Тестовая функция для проверки алгоритма работы с POST/GET-запросами."""
     # По задумке, в ответ на POST-запрос нужно вернуть JSON с теми данными,
     # которые получены в запросе.
     # Для этого в объект Response() передаём словарь request.data.
@@ -38,3 +42,25 @@ def hello(request):
     # В ответ на GET-запрос нужно вернуть JSON
     # Он тоже будет создан из словаря, переданного в Response()
     return Response({'message': 'Это был GET-запрос!'})
+
+
+@api_view(['PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+    """В случае PUT/PATCH-запроса изменяется объект класса Cat=pk.
+    В случае DELETE-запроса удаляется объект класса Cat=pk.
+    """
+    cat = Cat.objects.get(id=pk)
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            cat.delete()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer = CatSerializer(cat, many=False, partial=True)
+    return Response(serializer.data)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 
-from cats.views import cat_list
-from cats.views import hello
+from cats.views import cat_list, cat_detail, hello
 
 urlpatterns = [
    path('cats/', cat_list),
    path('hello/', hello),
+   path('cats/<int:pk>/', cat_detail),
 ]
